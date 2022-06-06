bf98297549bd72250e75d23adcc8cd78088ed6fa
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-06-06 10:52:06 +0900

    Add the computer move random selection.
    I couldn't fix the bug about the player's move. Still working on it.

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ var computer;
 var player;
 var playerMove = document.getElementById("player-move");
 var computerMove = document.getElementById("computer-move");
+const scissors = "fa-solid fa-hand-scissors";
+const rock = "fa-solid fa-hand-back-fist";
+const paper = "fa-solid fa-hand";
+const lizard = "fa-solid fa-hand-lizard";
+const spock = "fa-solid fa-hand-spock";
+
+const options = [scissors, rock, paper, lizard, spock];
 
 function playerChoice (e) {
     console.log(e.target.value);
-    console.log(e.target);
-    playerMove.innerHTML = e.target.innerHTML;
+    playerMove.className = e.target.value; 
+    computerMove.className = options[Math.floor(Math.random() * options.length)];
 }
 
 const playerBtns = document.querySelectorAll('.button-wrapper button');
