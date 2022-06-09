aab2a89120b6c13b665bda77eea21f931e0edb07
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-06-09 13:48:41 +0900

    Redefine the button logic to store variables. Can be optmized.

--- index.js
+++ index.js
@@ -2,37 +2,73 @@ var computer;
 var player;
 var playerMove = document.getElementById("player-move");
 var computerMove = document.getElementById("computer-move");
-const scissors = "fa-solid fa-hand-scissors";
-const rock = "fa-solid fa-hand-back-fist";
-const paper = "fa-solid fa-hand";
-const lizard = "fa-solid fa-hand-lizard";
-const spock = "fa-solid fa-hand-spock";
-const options = [scissors, rock, paper, lizard, spock];
+// const scissors = "fa-solid fa-hand-scissors";
+// const rock = "fa-solid fa-hand-back-fist";
+// const paper = "fa-solid fa-hand";
+// const lizard = "fa-solid fa-hand-lizard";
+// const spock = "fa-solid fa-hand-spock";
+// const options = [scissors, rock, paper, lizard, spock];
+
+const options = [
+    {
+        move: "scissors",
+        scheme: "fa-solid fa-hand-scissors"
+    },
+    {
+        move: "rock",
+        scheme: "fa-solid fa-hand-back-fist"
+    },
+    {
+        move: "paper",
+        scheme: "fa-solid fa-hand"
+    },
+    {
+        move: "lizard",
+        scheme: "fa-solid fa-hand-lizard"
+    },
+    {
+        move: "spock",
+        scheme: "fa-solid fa-hand-spock"
+    }
+
+]
 
 
-function playermove(choice) {
-    console.log(choice);
+function playermove(choice, move) {
+    player = move;
     playerMove.className = choice;
-    computerMove.className = options[Math.floor(Math.random() * options.length)];
-}
+    let index =  Math.floor(Math.random() * options.length);
+    computerMove.className = options[index].scheme;
+    computer = options[index].move;
+    console.log(computer);
+    // computermove();
+    
+};
+
+// function computermove() {
+//     let index =  Math.floor(Math.random() * options.length);
+//     computerMove.className = options[index].scheme;
+//     computer = options[index][move];
+//     console.groupCollapsed(computer);
+// };
 
 //the addEventListener should have a function pointer of a function returnin another function.
 //writing like addEventListener('click', oneFunction(argument)) will trigger the function right away.
 
 const paperbtn = document.getElementById("paper");
-paperbtn.addEventListener('click', function () { playermove("fa-solid fa-hand") });
+paperbtn.addEventListener('click', function () { playermove("fa-solid fa-hand", "paper") });
 
 const rockbtn = document.getElementById('rock');
-rockbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-back-fist") });
+rockbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-back-fist", "rock") });
 
 const scissorsbtn = document.getElementById('scissors');
-scissorsbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-scissors")})
+scissorsbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-scissors", "scissors")})
 
 const lizardbtn = document.getElementById('lizard');
-lizardbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-lizard")});
+lizardbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-lizard", "lizard")});
 
 const spockbtn = document.getElementById('spock');
-spockbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-spock")});
+spockbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-spock", "spock")});
 
 
 
