10620e1aa0a8e74c698f3e10d21579155854cdc5
Diego-Bittencourt <mendesdiego90@gmail.com>
2022-06-09 13:33:23 +0900

    Implement the buttons functionality.
    I successfully implemented the player's move buttons successfully.
    I created one variable to each button. This isn't the most efficient
    approach, but it works.
    
    I still couldn't make the jquery selector approach works, but still
    didn't give up on that.

--- index.js
+++ index.js
@@ -2,36 +2,46 @@ var computer;
 var player;
 var playerMove = document.getElementById("player-move");
 var computerMove = document.getElementById("computer-move");
-// const scissors = "fa-solid fa-hand-scissors";
-// const rock = "fa-solid fa-hand-back-fist";
-// const paper = "fa-solid fa-hand";
-// const lizard = "fa-solid fa-hand-lizard";
-// const spock = "fa-solid fa-hand-spock";
-
+const scissors = "fa-solid fa-hand-scissors";
+const rock = "fa-solid fa-hand-back-fist";
+const paper = "fa-solid fa-hand";
+const lizard = "fa-solid fa-hand-lizard";
+const spock = "fa-solid fa-hand-spock";
 const options = [scissors, rock, paper, lizard, spock];
 
+
 function playermove(choice) {
-    console.log("works");
+    console.log(choice);
     playerMove.className = choice;
     computerMove.className = options[Math.floor(Math.random() * options.length)];
 }
 
+//the addEventListener should have a function pointer of a function returnin another function.
+//writing like addEventListener('click', oneFunction(argument)) will trigger the function right away.
+
+const paperbtn = document.getElementById("paper");
+paperbtn.addEventListener('click', function () { playermove("fa-solid fa-hand") });
+
+const rockbtn = document.getElementById('rock');
+rockbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-back-fist") });
+
+const scissorsbtn = document.getElementById('scissors');
+scissorsbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-scissors")})
+
+const lizardbtn = document.getElementById('lizard');
+lizardbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-lizard")});
+
+const spockbtn = document.getElementById('spock');
+spockbtn.addEventListener('click', function () { playermove("fa-solid fa-hand-spock")});
+
+
+
+// ######## NOT WORKING CODE #############
 // function playerChoice (e) {
-//     console.log(e.target.value);
+//     console.log("works");
 //     playerMove.className = e.target.value; 
 //     computerMove.className = options[Math.floor(Math.random() * options.length)];
 // }
 
 // const playerBtns = document.querySelectorAll('.button-wrapper button');
-// playerBtns.forEach(onebutton => onebutton.addEventListener('click', playerChoice));
-
-const paperbtn = document.getElementById("paper");
-paperbtn.addEventListener("click", playermove("fa-solid fa-hand"));
-
-const rockbtn = document.getElementById('rock');
-rockbtn.addEventListener('click', playermove("fa-solid fa-hand-back-fist"));
-
-
-
-
-
+// playerBtns.forEach.onClick = playerChoice;
